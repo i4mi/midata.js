0e523fb21c5e3c0ac03c6cee6fe07a34a8080140
hafenr <robin.hafen@gmail.com>
2016-12-15 11:40:17 +0100

    Add jpeg as a valid image type

--- src/resources/ImageMedia.ts
+++ src/resources/ImageMedia.ts
@@ -1,20 +1,22 @@
 import { Media, Base64Str } from './Media';
 
 
+const supportedImageTypes = ['png', 'gif', 'jpg'];
+
 export type ImageType =
     'png' |
+    'jpg' |
     'gif' ;
 
 export class ImageMedia extends Media {
     constructor(filename: string, data: Base64Str) {
-        const supportedTypes = ['png', 'gif'];
         let type: string;
 
         let matches = filename.match(/\.(\w+)$/);
 
         if (matches !== null) {
             type = matches[1].toLowerCase();
-            if (supportedTypes.indexOf(type) === -1) {
+            if (supportedImageTypes.indexOf(type) === -1) {
                 throw new Error(`Unsupported type: ${type}`);
             }
         } else {
