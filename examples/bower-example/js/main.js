76959f1ff6242f7f1f8fdf1b883a6376f174e518
hafenr <robin.hafen@gmail.com>
2017-01-17 09:51:04 +0100

    Update bower example

--- examples/bower-example/js/main.js
+++ examples/bower-example/js/main.js
@@ -7,7 +7,15 @@ var resource = new midata.BodyWeight(23, new Date());
 // Login and subsequently try to save the resource on the server
 md.login('testuser@testuser.com', 'Testuser123')
 .then(() => {
-    midata.save(resource).then(response => {
+    md.save(resource).then(response => {
         console.log(response);
     });
+
+    // Search for resources
+    md.search('Observation', {
+        code: "http://loinc.org|6690-2"
+    }).then(function(resources) {
+        console.log('Got resources: ', resources);
+    });
 });
+
