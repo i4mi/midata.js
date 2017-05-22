be205c57ba573252ba59d7a4014d95c645312831
Adrian Wyss <adrianroman.wyss@bfh.ch>
2017-05-22 15:50:38 +0200

    add comments in Observation resource

--- src/resources/Observation.ts
+++ src/resources/Observation.ts
@@ -27,13 +27,15 @@ export class Observation extends Resource {
         this._fhir.component.push(component);
     }
 
-    // TODO: COMMENT
-
     addRelated(resource: any) {
         if (this._fhir['related'] == null) {
             this.addProperty('related', []);
         }
-
+        // Instances of type Resource defined by the midata.js library
+        // possess a reference method. Therefore, the relative reference can be build by invoking
+        // this method. Since the relative id consists of two values (resource type and resource id)
+        // concatenated by a slash (/), the method can also build the relative id directly during invocation.
+        // This presumes, however, that passed resources hold on to the FHIR base resource definition.
         let ref;
         if (resource instanceof Resource) {
             ref = resource.reference
@@ -60,7 +62,7 @@ export class ValueObservation extends Observation {
                 code: fhir.CodeableConcept,
                 category: fhir.CodeableConcept) {
         super(date, code, category);
-
+        // check type of value property
         if (this._isValueQuantity(valueType)) {
             this.addProperty('valueQuantity', valueType._quantity);
         } else if (this._isCodeableConcept(valueType)) {
@@ -70,15 +72,18 @@ export class ValueObservation extends Observation {
         }
     }
 
+    // Determine the type by using user-defined type guards.
+
     private _isValueQuantity(type: valueType): type is ValueQuantity {
         return (<ValueQuantity>type)._quantity !== undefined;
     }
-
     private _isCodeableConcept(type: valueType): type is CodeableConcept {
         return (<CodeableConcept>type)._codeableConcept !== undefined;
     }
 }
 ;
+
+// Definition of interfaces each declaring it's property with the corresponding value[x] type (see below).
 
 export interface ValueQuantity {
     _quantity: fhir.Quantity
@@ -87,6 +92,11 @@ export interface ValueQuantity {
 export interface CodeableConcept {
     _codeableConcept: fhir.CodeableConcept
 }
+
+// The information determined as a result of making the observation.
+// The element, Observation.value[x], has a variable name depending on the type as follows:
+// valueQuantity, valueCodeableConcept, valueRatio, valueChoice, valuePeriod, valueSampleData, or valueString
+// See https://www.hl7.org/fhir/observation-definitions.html#Observation.component.value_x_
 
 export type valueType =
 
